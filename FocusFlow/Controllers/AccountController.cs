f1d370cf613ba636e944f216c3c9316ddc844353
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-20 23:38:52 +0100

    Create account controller and add helper dependencies

---
+++ FocusFlow/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using FocusFlow.Data;
+using FocusFlow.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FocusFlow.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(ApplicationDbContext db,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
