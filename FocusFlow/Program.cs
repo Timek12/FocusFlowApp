f21e0eb33ef421df3f46a9cec9ced91fe4364354
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-20 20:44:37 +0100

    Add identity service

--- FocusFlow/Program.cs
+++ FocusFlow/Program.cs
@@ -1,4 +1,6 @@
 using FocusFlow.Data;
+using FocusFlow.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
