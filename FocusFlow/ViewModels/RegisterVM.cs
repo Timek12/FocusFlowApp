94737639830d7ff79b11f8a178c58765b1379009
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-21 00:39:14 +0100

    Add role properties and email datatype annotation

--- FocusFlow/ViewModels/RegisterVM.cs
+++ FocusFlow/ViewModels/RegisterVM.cs
@@ -1,10 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace FocusFlow.ViewModels
 {
     public class RegisterVM
     {
         [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Required]
@@ -18,9 +21,13 @@ namespace FocusFlow.ViewModels
         public string ConfirmPassword { get; set; }
 
         [Required]
+        [Display(Name = "Full Name")]
         public string Name { get; set; }
         [Display(Name = "Phone Number")]
         public string? PhoneNumber { get; set; }
         public string? RedirectUrl { get; set; }
+        public string? Role {  get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? RoleList { get; set; }
     }
 }
