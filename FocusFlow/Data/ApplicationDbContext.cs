5f74c779134ecbb623e66854daa62f45e3b010ae
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-20 18:53:50 +0100

    Create ApplicationDbContext file

---
+++ FocusFlow/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FocusFlow.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FocusFlow.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    }
+}
