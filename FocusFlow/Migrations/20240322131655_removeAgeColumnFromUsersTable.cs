46a25a95fdf73edca2b4bba775e88539614a8553
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-22 14:17:40 +0100

    Remove age column from users table

---
+++ FocusFlow/Migrations/20240322131655_removeAgeColumnFromUsersTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FocusFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeAgeColumnFromUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- FocusFlow/Models/ApplicationUser.cs
+++ FocusFlow/Models/ApplicationUser.cs
@@ -5,7 +5,6 @@ namespace FocusFlow.Models
     public class ApplicationUser : IdentityUser
     {
         public string Name { get; set; }
-        public int Age { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 }
