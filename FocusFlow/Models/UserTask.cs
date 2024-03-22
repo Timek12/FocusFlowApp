24941c58ccb4733cd6113b5074057c0f0f3eb30a
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-22 22:25:05 +0100

    Create UserTask model

---
+++ FocusFlow/Models/UserTask.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static FocusFlow.Utility.SD;
+
+namespace FocusFlow.Models
+{
+    public class UserTask
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Utility.SD.TaskStatus Status { get; set; }
+        public TaskImportance Importance { get; set; } = 0;
+        public DateTime CreatedAt { get; set; }
+        public DateTime Deadline { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+    }
+}
