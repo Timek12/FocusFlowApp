fb971ead71558db837ead88270d6fabfad201df3
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-22 22:24:47 +0100

    Add TaskStatus and TaskImportance enums

--- FocusFlow/Utility/SD.cs
+++ FocusFlow/Utility/SD.cs
@@ -4,5 +4,18 @@
     {
         public const string Role_User = "User";
         public const string Role_Admin = "Admin";
+
+        public enum TaskStatus
+        {
+            InProgress,
+            Completed
+        }
+
+        public enum TaskImportance
+        {
+            Low,
+            Medium,
+            High
+        }
     }
 }
