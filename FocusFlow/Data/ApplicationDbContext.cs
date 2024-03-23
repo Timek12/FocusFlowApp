faeefa0a1d01e17ebea8491dfb6601de09b13252
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-23 21:09:37 +0100

    Add Tasks table and seed it with sample data

--- FocusFlow/Data/ApplicationDbContext.cs
+++ FocusFlow/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using FocusFlow.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using static FocusFlow.Utility.SD;
 
 namespace FocusFlow.Data
 {
@@ -8,6 +9,21 @@ namespace FocusFlow.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+        public DbSet<UserTask> Tasks { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserTask>().HasData(
+                new UserTask { TaskId = 1, Name = "Design Homepage", Description = "Create a responsive design for the homepage", Status = Utility.SD.TaskStatus.InProgress, Importance = TaskImportance.Medium, CreatedAt = DateTime.Now, Deadline = DateTime.Now.AddDays(7), UserId = "1ebdc38a-a0ce-491d-a1fd-22c2ea0bd598" },
+                new UserTask { TaskId = 2, Name = "Database Migration", Description = "Migrate the existing database to the new server", Status = Utility.SD.TaskStatus.InProgress, Importance = TaskImportance.High, CreatedAt = DateTime.Now, Deadline = DateTime.Now.AddDays(3), UserId = "1ebdc38a-a0ce-491d-a1fd-22c2ea0bd598" },
+                new UserTask { TaskId = 3, Name = "Bug Fixing", Description = "Fix the reported bugs in the user management module", Status = Utility.SD.TaskStatus.InProgress, Importance = TaskImportance.Low, CreatedAt = DateTime.Now, Deadline = DateTime.Now.AddDays(12), UserId = "1ebdc38a-a0ce-491d-a1fd-22c2ea0bd598" },
+                new UserTask { TaskId = 4, Name = "API Integration", Description = "Integrate the payment gateway API", Status = Utility.SD.TaskStatus.InProgress, Importance = TaskImportance.Low, CreatedAt = DateTime.Now, Deadline = DateTime.Now.AddDays(10), UserId = "2691dfbb-9544-4aed-8893-b3b19f6d2af1" },
+                new UserTask { TaskId = 5, Name = "Performance Testing", Description = "Conduct performance testing on the new release", Status = Utility.SD.TaskStatus.InProgress, Importance = TaskImportance.Medium, CreatedAt = DateTime.Now, Deadline = DateTime.Now.AddDays(1), UserId = "2691dfbb-9544-4aed-8893-b3b19f6d2af1" },
+                new UserTask { TaskId = 6, Name = "Code Review", Description = "Review the code of the new features", Status = Utility.SD.TaskStatus.InProgress, Importance = TaskImportance.High, CreatedAt = DateTime.Now, Deadline = DateTime.Now.AddDays(14), UserId = "2691dfbb-9544-4aed-8893-b3b19f6d2af1" }
+            );
+        }
     }
 }
---
+++ FocusFlow/Migrations/20240323200813_addTasksTableAndSeedWithSampleData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FocusFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTasksTableAndSeedWithSampleData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Importance = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Deadline = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.TaskId);
+                    table.ForeignKey(
+                        name: "FK_Tasks_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "TaskId", "CreatedAt", "Deadline", "Description", "Importance", "Name", "Status", "UserId" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 3, 23, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(369), new DateTime(2024, 3, 30, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(413), "Create a responsive design for the homepage", 1, "Design Homepage", 0, "1ebdc38a-a0ce-491d-a1fd-22c2ea0bd598" },
+                    { 2, new DateTime(2024, 3, 23, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(418), new DateTime(2024, 3, 26, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(419), "Migrate the existing database to the new server", 2, "Database Migration", 0, "1ebdc38a-a0ce-491d-a1fd-22c2ea0bd598" },
+                    { 3, new DateTime(2024, 3, 23, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(421), new DateTime(2024, 4, 4, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(422), "Fix the reported bugs in the user management module", 0, "Bug Fixing", 0, "1ebdc38a-a0ce-491d-a1fd-22c2ea0bd598" },
+                    { 4, new DateTime(2024, 3, 23, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(424), new DateTime(2024, 4, 2, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(424), "Integrate the payment gateway API", 0, "API Integration", 0, "2691dfbb-9544-4aed-8893-b3b19f6d2af1" },
+                    { 5, new DateTime(2024, 3, 23, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(426), new DateTime(2024, 3, 24, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(427), "Conduct performance testing on the new release", 1, "Performance Testing", 0, "2691dfbb-9544-4aed-8893-b3b19f6d2af1" },
+                    { 6, new DateTime(2024, 3, 23, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(429), new DateTime(2024, 4, 6, 21, 8, 13, 387, DateTimeKind.Local).AddTicks(430), "Review the code of the new features", 2, "Code Review", 0, "2691dfbb-9544-4aed-8893-b3b19f6d2af1" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_UserId",
+                table: "Tasks",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
