aef5e0aec501482ad4c5555e439331f94be8d200
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-24 00:52:44 +0100

    Create UserTask view model

---
+++ FocusFlow/ViewModels/UserTaskVM.cs
@@ -0,0 +1,15 @@
+﻿using FocusFlow.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FocusFlow.ViewModels
+{
+    public class UserTaskVM
+    {
+        public UserTask? UserTask { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> StatusList { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> ImportanceList { get; set; }
+    }
+}
