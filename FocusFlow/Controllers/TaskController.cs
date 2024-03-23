a16317f4d4129ba63c42db87a5dbf6edb1732092
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-23 21:48:52 +0100

    Create Task controller with index endpoint

---
+++ FocusFlow/Controllers/TaskController.cs
@@ -0,0 +1,21 @@
+﻿using FocusFlow.Data;
+using FocusFlow.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FocusFlow.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TaskController(ApplicationDbContext db)
+        {
+            _db = db;            
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<UserTask> tasks = _db.Tasks;
+            return View(tasks);
+        }
+    }
+}
