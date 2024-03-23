c65fee50991862c867f196f68bdd820a2c5dba9e
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-24 00:53:21 +0100

    Add 'Create' get endpoint and 'Update' get endpoint

--- FocusFlow/Controllers/TaskController.cs
+++ FocusFlow/Controllers/TaskController.cs
@@ -1,6 +1,10 @@
 ﻿using FocusFlow.Data;
 using FocusFlow.Models;
+using FocusFlow.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using static FocusFlow.Utility.SD;
 
 namespace FocusFlow.Controllers
 {
@@ -16,6 +20,43 @@ namespace FocusFlow.Controllers
         {
             IEnumerable<UserTask> tasks = _db.Tasks;
             return View(tasks);
+        }
+
+        public IActionResult Create()
+        {
+            UserTaskVM userTaskVM = new()
+            {
+                StatusList = Enum.GetValues(typeof(Utility.SD.TaskStatus))
+                .Cast<Utility.SD.TaskStatus>().Select(e => new SelectListItem
+                 {
+                    Value = ((int)e).ToString(),
+                    Text = e.ToString()
+                 }),
+                ImportanceList = Enum.GetValues(typeof(TaskImportance))
+                .Cast<TaskImportance>().Select(e => new SelectListItem
+                {
+                    Value = ((int)e).ToString(),
+                    Text = e.ToString()
+                }),
+            };
+
+            return View(userTaskVM);
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if(id < 1)
+            {
+                RedirectToAction("Error", "Home");
+            }
+
+            UserTask? taskFromDb = await _db.Tasks.FirstOrDefaultAsync(u => u.TaskId == id);
+            if (taskFromDb == null)
+            {
+                RedirectToAction("Error", "Home");
+            }
+
+            return View(taskFromDb);
         }
     }
 }
