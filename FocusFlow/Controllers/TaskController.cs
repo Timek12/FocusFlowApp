b868aae9247a4604a2b6b3dd1898524a61e6f030
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-24 20:58:31 +0100

    Add userManager service and create Create task post endpoint

--- FocusFlow/Controllers/TaskController.cs
+++ FocusFlow/Controllers/TaskController.cs
@@ -5,15 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using static FocusFlow.Utility.SD;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace FocusFlow.Controllers
 {
+    [Authorize]
     public class TaskController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public TaskController(ApplicationDbContext db)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public TaskController(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
         {
-            _db = db;            
+            _db = db;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -28,10 +33,10 @@ namespace FocusFlow.Controllers
             {
                 StatusList = Enum.GetValues(typeof(Utility.SD.TaskStatus))
                 .Cast<Utility.SD.TaskStatus>().Select(e => new SelectListItem
-                 {
+                {
                     Value = ((int)e).ToString(),
                     Text = e.ToString()
-                 }),
+                }),
                 ImportanceList = Enum.GetValues(typeof(TaskImportance))
                 .Cast<TaskImportance>().Select(e => new SelectListItem
                 {
@@ -43,9 +48,47 @@ namespace FocusFlow.Controllers
             return View(userTaskVM);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Create(UserTaskVM userTaskVM)
+        {
+            if (ModelState.IsValid)
+            {
+                UserTask userTask = new()
+                {
+                    Name = userTaskVM.Name,
+                    Description = userTaskVM.Description,
+                    Status = userTaskVM.Status,
+                    Importance = userTaskVM.Importance,
+                    CreatedAt = DateTime.Now,
+                    Deadline = userTaskVM.Deadline,
+                    UserId = _userManager.GetUserId(User)
+                };
+
+                _db.Tasks.Add(userTask);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            userTaskVM.StatusList = Enum.GetValues(typeof(Utility.SD.TaskStatus))
+                .Cast<Utility.SD.TaskStatus>().Select(e => new SelectListItem
+                {
+                    Value = ((int)e).ToString(),
+                    Text = e.ToString()
+                });
+
+            userTaskVM.ImportanceList = Enum.GetValues(typeof(TaskImportance))
+            .Cast<TaskImportance>().Select(e => new SelectListItem
+            {
+                Value = ((int)e).ToString(),
+                Text = e.ToString()
+            });
+
+            return View(userTaskVM);
+        }
+
         public async Task<IActionResult> Update(int? id)
         {
-            if(id < 1)
+            if (id < 1)
             {
                 RedirectToAction("Error", "Home");
             }
