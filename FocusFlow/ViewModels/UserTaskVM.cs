42e230e4e19c1022da03710a67d04fa072058236
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-24 20:58:02 +0100

    Remove UserTask property and add only neccessary fields

--- FocusFlow/ViewModels/UserTaskVM.cs
+++ FocusFlow/ViewModels/UserTaskVM.cs
@@ -2,12 +2,19 @@
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel;
+using static FocusFlow.Utility.SD;
+using System.ComponentModel.DataAnnotations;
 
 namespace FocusFlow.ViewModels
 {
     public class UserTaskVM
     {
-        public UserTask? UserTask { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Utility.SD.TaskStatus Status { get; set; }
+        public TaskImportance Importance { get; set; } = 0;
+        public DateTime Deadline { get; set; }
         [ValidateNever]
         [DisplayName("Status")]
         public IEnumerable<SelectListItem> StatusList { get; set; }
