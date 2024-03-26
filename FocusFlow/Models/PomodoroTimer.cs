34ee976dbd428f16489b413bbfd18977c5b4b99d
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-26 01:23:01 +0100

    Create PomodoroTimer model

---
+++ FocusFlow/Models/PomodoroTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FocusFlow.Models
+{
+    public class PomodoroTimer
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public TimeSpan BreakDuration { get; set; }
+        public bool isRunning { get; set; }
+        public bool isBreak {  get; set; }
+        public List<UserTask>? Tasks { get; set; }
+        
+        public string UserId { get; set; }
+    }
+}
