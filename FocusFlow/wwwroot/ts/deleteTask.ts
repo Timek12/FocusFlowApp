625d8ec47929ee593d48b0ebab06bbc26c60696e
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-27 15:25:15 +0100

    Rewrite deleteTaks js file to typescript

---
+++ FocusFlow/wwwroot/ts/deleteTask.ts
@@ -0,0 +1,40 @@
+﻿import Swal from 'sweetalert2'
+$(document).ready(function () {
+    $('.delete-task').click(function () {
+        const taskId: string = $(this).data('id');
+        console.log(taskId)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result: any) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: '/Task/Delete',
+                    type: 'POST',
+                    data: { id: taskId },
+                    success: () => {
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                        }).then(() => {
+                            location.reload();
+                        });
+                    },
+                    error: () => {
+                        Swal.fire({
+                            title: "Error!",
+                            text: "Something went wrong.",
+                            icon: "error"
+                        });
+                    }
+                })
+            }
+        });
+    })
+})
