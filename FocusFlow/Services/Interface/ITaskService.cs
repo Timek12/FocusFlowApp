be9cb063486c01e1b65c6500ba590453b7eac842
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-28 19:44:49 +0100

    Create TaskService interface

---
+++ FocusFlow/Services/Interface/ITaskService.cs
@@ -0,0 +1,12 @@
+﻿namespace FocusFlow.Services.Interface
+{
+    public interface ITaskService
+    {
+        IEnumerable<Task> GetAllTasks(string userId);
+        Task GetTaskById(int taskId);
+
+        void AddTask(Task task);
+        void UpdateTask(Task task);
+        void RemoveTask(Task task);
+    }
+}
