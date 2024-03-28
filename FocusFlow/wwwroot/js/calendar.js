612ec7ddaa16771f5fb68c74ee0bde36fec7b4f9
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-28 17:07:32 +0100

    Add fixedWeekCount and height configuration, add darkgrey calendarTheme

--- FocusFlow/wwwroot/js/calendar.js
+++ FocusFlow/wwwroot/js/calendar.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
-    console.log(calendarEl); // Should not be null
     var calendar = new FullCalendar.Calendar(calendarEl, {
+        themeSystem: 'bootstrap5',
         initialView: 'dayGridMonth',
-        firstDay: 1 // Monday
+        firstDay: 1, // Monday
+        fixedWeekCount: false,
+        height: '700px',
     });
     calendar.render();
 });
--- FocusFlow/wwwroot/ts/calendar.ts
+++ FocusFlow/wwwroot/ts/calendar.ts
@@ -1,12 +1,13 @@
 ﻿declare var FullCalendar: any;
-
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
-    console.log(calendarEl); // Should not be null
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
+        themeSystem: 'bootstrap5',
         initialView: 'dayGridMonth',
-        firstDay: 1 // Monday
+        firstDay: 1, // Monday
+        fixedWeekCount: false,
+        height: '700px',
     });
     calendar.render();
 });
