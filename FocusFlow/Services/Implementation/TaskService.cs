ccdb63752ca433db4884fa4a5a050efddf34f4ef
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-28 20:07:56 +0100

    Add async methods, implement all TaskService methods

---
+++ FocusFlow/Services/Implementation/TaskService.cs
@@ -0,0 +1,44 @@
+﻿using FocusFlow.Data;
+using FocusFlow.Models;
+using FocusFlow.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace FocusFlow.Services.Implementation
+{
+    public class TaskService : ITaskService
+    {
+        private readonly ApplicationDbContext _db;
+        public TaskService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddTask(UserTask task)
+        {
+            _db.Tasks.Add(task);
+            await _db.SaveChangesAsync();
+        }
+
+        public IEnumerable<UserTask> GetAllTasks(string userId, bool isAdmin)
+        {
+            return isAdmin ? _db.Tasks : _db.Tasks.Where(u => u.UserId == userId);
+        }
+
+        public async Task<UserTask> GetTaskById(int taskId)
+        {
+            return await _db.Tasks.FirstOrDefaultAsync(u => u.TaskId == taskId);
+        }
+
+        public async Task RemoveTask(UserTask task)
+        {
+            _db.Tasks.Remove(task);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task UpdateTask(UserTask task)
+        {
+            _db.Tasks.Update(task);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
--- FocusFlow/Services/Interface/ITaskService.cs
+++ FocusFlow/Services/Interface/ITaskService.cs
@@ -5,10 +5,10 @@ namespace FocusFlow.Services.Interface
     public interface ITaskService
     {
         IEnumerable<UserTask> GetAllTasks(string userId, bool isAdmin);
-        UserTask GetTaskById(int taskId);
+        Task<UserTask> GetTaskById(int taskId);
 
-        void AddTask(UserTask task);
-        void UpdateTask(UserTask task);
-        void RemoveTask(UserTask task);
+        Task AddTask(UserTask task);
+        Task UpdateTask(UserTask task);
+        Task RemoveTask(UserTask task);
     }
 }
