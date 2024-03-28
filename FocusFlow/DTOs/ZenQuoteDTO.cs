320eceef88a1c365c2fcedf2dda5ae6dedebfd64
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-29 00:03:42 +0100

    Create ZenQuoteDTO

---
+++ FocusFlow/DTOs/ZenQuoteDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FocusFlow.DTOs
+{
+    public class ZenQuoteDTO
+    {
+        public string q { get; set; } // quote 
+        public string a { get; set; } // author
+        public string i { get; set; } // author image
+        public string c { get; set; } // character count
+        public string h { get; set; } // pre-formatted html quote
+    }
+}
