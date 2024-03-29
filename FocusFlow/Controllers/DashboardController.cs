1ffd398811c74406602075e7aa17ad344fd7401c
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-29 17:01:36 +0100

    Create Dasboard controller and view, add Dashboard nav-item to navbar

---
+++ FocusFlow/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using FocusFlow.Data;
+using FocusFlow.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FocusFlow.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DashboardController(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
