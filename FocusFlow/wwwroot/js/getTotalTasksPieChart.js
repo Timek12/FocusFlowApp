31f83246babb584c624f6245d19fe89d35ce9872
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-31 18:10:29 +0200

    Create script for creating Total Tasks pie chart using apex charts library

---
+++ FocusFlow/wwwroot/js/getTotalTasksPieChart.js
@@ -0,0 +1,41 @@
+var ApexCharts;
+$(function () {
+    loadTotalTasksPieChartData();
+});
+function loadTotalTasksPieChartData() {
+    fetch("/Dashboard/GetTasksPieChartData", {
+        method: 'GET',
+    })
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
+        var options = {
+            chart: {
+                type: 'pie'
+            },
+            series: data.series,
+            labels: data.labels,
+            colors: ['#32CD32', '#B0E0E6'],
+            title: {
+                text: 'Total Tasks',
+                align: 'center',
+                style: {
+                    fontSize: '14px',
+                    fontWeight: 'bold',
+                    color: '#263238'
+                },
+            },
+            legend: {
+                position: 'bottom',
+                horizontalAlign: 'center',
+                labels: {
+                    colors: "#fff",
+                    userSeriesColors: true
+                },
+            },
+        };
+        var chart = new ApexCharts(document.querySelector("#totalTasksPieChart"), options);
+        chart.render();
+    })
+        .catch(function (error) { return console.error('Error: ', error); });
+}
+//# sourceMappingURL=getTotalTasksPieChart.js.map
---
+++ FocusFlow/wwwroot/ts/getTotalTasksPieChart.ts
@@ -0,0 +1,42 @@
+﻿var ApexCharts: any;
+$(function () {
+    loadTotalTasksPieChartData();
+});
+
+function loadTotalTasksPieChartData() {
+    fetch("/Dashboard/GetTasksPieChartData", {
+        method: 'GET',
+    })
+        .then(response => response.json())
+        .then(data => {
+            var options = {
+                chart: {
+                    type: 'pie'
+                },
+                series: data.series,
+                labels: data.labels,
+                colors: ['#32CD32', '#B0E0E6'],
+                title: {
+                    text: 'Total Tasks',
+                    align: 'center',
+                    style: {
+                        fontSize: '14px',
+                        fontWeight: 'bold',
+                        color: '#263238'
+                    },
+                },
+                legend: {
+                    position: 'bottom',
+                    horizontalAlign: 'center',
+                    labels: {
+                        colors: "#fff",
+                        userSeriesColors: true
+                    },
+                },
+            }
+
+            var chart = new ApexCharts(document.querySelector("#totalTasksPieChart"), options);
+            chart.render();
+        })
+        .catch(error => console.error('Error: ', error));
+}
