0de36d2ed0450f6bb4f3176aa7e3ab0daa427b31
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-31 18:40:06 +0200

    Create Total Tasks Importance pie chart

---
+++ FocusFlow/wwwroot/js/dashboard/getTotalTasksImportancePieChart.js
@@ -0,0 +1,41 @@
+var ApexCharts;
+$(function () {
+    loadTotalTasksImportancePieChart();
+});
+function loadTotalTasksImportancePieChart() {
+    fetch("/Dashboard/GetTasksImportancePieChartData", {
+        method: 'GET',
+    })
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
+        var options = {
+            chart: {
+                type: 'pie'
+            },
+            series: data.series,
+            labels: data.labels,
+            colors: ['#28a745', '#ffc107', '#dc3545'],
+            title: {
+                text: 'Total Tasks Importance',
+                align: 'center',
+                style: {
+                    fontSize: '14px',
+                    fontWeight: 'bold',
+                    color: '#263238'
+                },
+            },
+            legend: {
+                position: 'bottom',
+                horizontalAlign: 'center',
+                labels: {
+                    colors: "#fff",
+                    userSeriesColors: true
+                },
+            },
+        };
+        var chart = new ApexCharts(document.querySelector("#totalTasksImportancePieChart"), options);
+        chart.render();
+    })
+        .catch(function (error) { return console.error('Error: ', error); });
+}
+//# sourceMappingURL=getTotalTasksImportancePieChart.js.map
---
+++ FocusFlow/wwwroot/ts/dashboard/getTotalTasksImportancePieChart.ts
@@ -0,0 +1,42 @@
+﻿var ApexCharts: any;
+$(function () {
+    loadTotalTasksImportancePieChart();
+});
+
+function loadTotalTasksImportancePieChart() {
+    fetch("/Dashboard/GetTasksImportancePieChartData", {
+        method: 'GET',
+    })
+        .then(response => response.json())
+        .then(data => {
+            var options = {
+                chart: {
+                    type: 'pie'
+                },
+                series: data.series,
+                labels: data.labels,
+                colors: ['#28a745', '#ffc107', '#dc3545'],
+                title: {
+                    text: 'Total Tasks Importance',
+                    align: 'center',
+                    style: {
+                        fontSize: '14px',
+                        fontWeight: 'bold',
+                        color: '#263238'
+                    },
+                },
+                legend: {
+                    position: 'bottom',
+                    horizontalAlign: 'center',
+                    labels: {
+                        colors: "#fff",
+                        userSeriesColors: true
+                    },
+                },
+            }
+
+            var chart = new ApexCharts(document.querySelector("#totalTasksImportancePieChart"), options);
+            chart.render();
+        })
+        .catch(error => console.error('Error: ', error));
+}
