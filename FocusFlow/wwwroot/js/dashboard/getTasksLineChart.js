f82c6b8be754e7f1967b57509a2829b2ea0a9f4b
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-31 23:04:49 +0200

    Create last 30 days tasks line chart

---
+++ FocusFlow/wwwroot/js/dashboard/getTasksLineChart.js
@@ -0,0 +1,56 @@
+var ApexCharts;
+$(function () {
+    loadTasksLineChart();
+});
+function loadTasksLineChart() {
+    fetch("/Dashboard/GetTasksLineChartData", {
+        method: 'GET',
+    })
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
+        var options = {
+            series: data.series,
+            chart: {
+                height: 350,
+                type: 'line',
+            },
+            stroke: {
+                curve: "smooth",
+            },
+            markers: {
+                size: 6,
+                strokeWidth: 0,
+                hover: {
+                    size: 7
+                }
+            },
+            xaxis: {
+                categories: data.categories,
+                labels: {
+                    style: {
+                        colors: "#ddd",
+                    },
+                }
+            },
+            yaxis: {
+                labels: {
+                    style: {
+                        colors: "#fff",
+                    },
+                }
+            },
+            legend: {
+                labels: {
+                    colors: "#fff",
+                },
+            },
+            tooltip: {
+                theme: 'dark'
+            }
+        };
+        var chart = new ApexCharts(document.querySelector("#getTasksLineChart"), options);
+        chart.render();
+    })
+        .catch(function (error) { return console.error('Error: ', error); });
+}
+//# sourceMappingURL=getTasksLineChart.js.map
---
+++ FocusFlow/wwwroot/ts/dashboard/getTasksLineChart.ts
@@ -0,0 +1,57 @@
+﻿var ApexCharts: any;
+$(function () {
+    loadTasksLineChart();
+});
+
+function loadTasksLineChart() {
+    fetch("/Dashboard/GetTasksLineChartData", {
+        method: 'GET',
+    })
+        .then(response => response.json())
+        .then(data => {
+            var options = {
+                series: data.series,
+                chart: {
+                    height: 350,
+                    type: 'line',
+                },
+                stroke: {
+                    curve: "smooth",
+                },
+                markers: {
+                    size: 6,
+                    strokeWidth: 0,
+                    hover: {
+                        size: 7
+                    }
+                },
+                xaxis: {
+                    categories: data.categories,
+                    labels: {
+                        style: {
+                            colors: "#ddd",
+                        },
+                    }
+                },
+                yaxis: {
+                    labels: {
+                        style: {
+                            colors: "#fff",
+                        },
+                    }
+                },
+                legend: {
+                    labels: {
+                        colors: "#fff",
+                    },
+                },
+                tooltip: {
+                    theme: 'dark'
+                }
+            };
+
+            var chart = new ApexCharts(document.querySelector("#getTasksLineChart"), options);
+            chart.render();
+        })
+        .catch(error => console.error('Error: ', error));
+}
