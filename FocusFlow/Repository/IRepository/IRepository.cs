a591068eba476c53164a7fbd24922d6b6112bc11
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-02 00:20:59 +0200

    Add generic repository implementation and interface

---
+++ FocusFlow/Repository/IRepository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace FocusFlow.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, bool tracked = false);
+        T? Get(Expression<Func<T, bool>> filter, bool tracked = false);
+        void Add(T item);
+        void Remove(T item);
+    }
+}
---
+++ FocusFlow/Repository/Repository.cs
@@ -0,0 +1,56 @@
+﻿using FocusFlow.Data;
+using FocusFlow.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FocusFlow.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            dbSet = _db.Set<T>();
+        }
+
+        public void Add(T item)
+        {
+            dbSet.Add(item);
+        }
+
+        public T? Get(Expression<Func<T, bool>> filter, bool tracked = false)
+        {
+            IQueryable<T> query = dbSet.AsQueryable();
+
+            if(!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            query = query.Where(filter);
+
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T item)
+        {
+            dbSet.Remove(item);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, bool tracked = false)
+        {
+            IQueryable<T> query = dbSet.AsQueryable();
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            query = query.Where(filter);
+
+            return query.ToList();
+        }
+    }
+}
