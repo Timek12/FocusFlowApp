634345d952b50b16e39abb5ef0f832865f9cc2c2
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-02 00:45:55 +0200

    Create Task service implementation and interface

---
+++ FocusFlow/Services/Implementation/TaskService.cs
@@ -0,0 +1,49 @@
+﻿using FocusFlow.Models;
+using FocusFlow.Repository.IRepository;
+using FocusFlow.Services.Interface;
+
+namespace FocusFlow.Services.Implementation
+{
+    public class TaskService : ITaskService
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public TaskService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void AddTask(UserTask task)
+        {
+            _unitOfWork.Task.Add(task);
+            _unitOfWork.Save();
+        }
+
+        public IEnumerable<UserTask> GetAllTasks(string userId, bool isAdmin)
+        {
+            return isAdmin ? _unitOfWork.Task.GetAll() : _unitOfWork.Task.GetAll(u => u.UserId == userId);
+        }
+
+        public UserTask? GetTaskById(int taskId)
+        {
+            return _unitOfWork.Task.Get(u => u.TaskId == taskId);
+        }
+
+        public IQueryable<UserTask> GetUserTasksQuery()
+        {
+            return _unitOfWork.Task.GetQuery();
+        }
+
+        public void RemoveTask(UserTask task)
+        {
+            _unitOfWork.Task.Remove(task);
+            _unitOfWork.Save();
+        }
+
+        public void UpdateTask(UserTask task)
+        {
+            _unitOfWork.Task.Update(task);
+            _unitOfWork.Save();
+        }
+    }
+}
---
+++ FocusFlow/Services/Interface/ITaskService.cs
@@ -0,0 +1,14 @@
+﻿using FocusFlow.Models;
+
+namespace FocusFlow.Services.Interface
+{
+    public interface ITaskService
+    {
+        IEnumerable<UserTask> GetAllTasks(string userId, bool isAdmin);
+        UserTask? GetTaskById(int taskId);
+        IQueryable<UserTask> GetUserTasksQuery();
+        void AddTask(UserTask task);
+        void UpdateTask(UserTask task);
+        void RemoveTask(UserTask task);
+    }
+}
